cb989edab05748c0a5cffa3f21f2b08e4f37db08
Harish V A <vaharish@gmail.com>
2014-05-06 22:04:28 +0530

    kernel thread example

---
+++ kthread/kthread.c
@@ -0,0 +1,33 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/kthread.h>
+#include<linux/sched.h>
+
+struct task_struct *task;
+int data;
+int ret;
+int thread_function(void *data)
+{
+    int var;
+    var = 10;
+    return var;
+}
+
+static int kernel_init(void)
+{
+    data = 20;
+    printk(KERN_INFO"--------------------------------------------");
+    task = kthread_create(&thread_function,(void *)data,"harish");
+    task = kthread_run(&thread_function,(void *)data,"harish");
+    printk(KERN_INFO"Kernel Thread : %s\n",task->comm);
+    return 0;
+}
+
+static void kernel_exit(void)
+{
+    ret = kthread_stop(task);
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
