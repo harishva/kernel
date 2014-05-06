2871c48bec87c24c328e04bbf7fabef3d78e1e77
Harish V A <vaharish@gmail.com>
2014-05-06 22:39:26 +0530

    first kernel module

---
+++ pscode/pscode.c
@@ -0,0 +1,25 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+
+
+MODULE_LICENSE("GPL");
+
+static int pscode_init(void)
+{
+	printk(KERN_INFO "Initializing Pscode Module\n");
+	return 0;
+}
+
+static void pscode_exit(void)
+{
+	printk(KERN_INFO "Exiting Pscode Module\n");
+}
+
+module_init(pscode_init);
+module_exit(pscode_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("HARISH");
+MODULE_DESCRIPTION("Kernal first module ");
+MODULE_SUPPORTED_DEVICE("test module");
